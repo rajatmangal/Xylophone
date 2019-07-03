9239a0fa9a35ca71fd1c96af15ab8e6a2bc0de01
Rajat Mangal <rajatmangal@Rajats-MBP.hitronhub.home>
2019-07-02 21:25:04 -0700

    Differnet sounds added

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,58 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-                onPressed: (){
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text("click me")
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note1.wav');
+                    },
+                    child: Text("Note 1")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note2.wav');
+                    },
+                    child: Text("Note 2")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note3.wav');
+                    },
+                    child: Text("Note 3")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note4.wav');
+                    },
+                    child: Text("Note 4")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note5.wav');
+                    },
+                    child: Text("Note 5")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note6.wav');
+                    },
+                    child: Text("Note 6")
+                ),
+                FlatButton(
+                    onPressed: (){
+                      final player = AudioCache();
+                      player.play('note7.wav');
+                    },
+                    child: Text("Note 7")
+                ),
+              ],
             ),
           ),
         ),
